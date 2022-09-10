d94667ec49757114ee13af244f7fde4811e29a1e
Oly-Kim <oly-kim@email.com>
2022-09-11 02:37:30 +0900

    1001번 문제

---
+++ BaekJoon/History/Problem1002.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.History
+{
+    public class Problem1002
+    {
+        public static void Submit()
+        {
+
+        }
+    }
+}
