cb19ea10503c9e1c9c4045bb7df8c3f2045d2382
Oly-Kim <oly-kim@email.com>
2022-09-11 03:29:14 +0900

    1008번 문제

--- BaekJoon/History/Problem1008.cs
+++ BaekJoon/History/Problem1008.cs
@@ -1,8 +1,14 @@
-﻿namespace BaekJoon
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.History
 {
-    internal class Program
+    public static class Problem1008
     {
-        static void Main(string[] args)
+        public static void Submit()
         {
             //두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
 
@@ -16,7 +22,7 @@
             string[] inputMessage = Console.ReadLine().Split();
             double a = double.Parse(inputMessage[0]);
             double b = double.Parse(inputMessage[1]);
-            double dv = a/b;
+            double dv = a / b;
             Console.WriteLine("{0:f12}", dv);
         }
     }
