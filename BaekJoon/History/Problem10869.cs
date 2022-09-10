d6123c501f499876a2d70f2279159b426efae8bb
Oly-Kim <oly-kim@email.com>
2022-09-11 04:18:02 +0900

    10869번 문제

--- BaekJoon/History/Problem10869.cs
+++ BaekJoon/History/Problem10869.cs
@@ -1,8 +1,14 @@
-﻿namespace BaekJoon
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.History
 {
-    internal class Program
+    public  class Problem10869
     {
-        static void Main(string[] args)
+        public static void Submit()
         {
             //두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
             //두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
@@ -25,8 +31,7 @@
             int addition = Int32.Parse(a) + Int32.Parse(b);
             int subtraction = Int32.Parse(a) - Int32.Parse(b);
             int multiplication = Int32.Parse(a) * Int32.Parse(b);
-            //double division = double.Parse(a) / double.Parse(b);
-            int division = int.Parse(a) / int.Parse(b);
+            double division = double.Parse(a) / double.Parse(b);
             int quotient = Int32.Parse(a) % Int32.Parse(b);
             Console.WriteLine(addition);
             Console.WriteLine(subtraction);
@@ -34,5 +39,6 @@
             Console.WriteLine(division);
             Console.WriteLine(quotient);
         }
+        
     }
 }
